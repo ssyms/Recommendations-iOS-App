d8905231e195839ba49c45de01270e50d8c4a6ab
Sarada Symonds <symonds.sarada@gmail.com>
2015-07-28 13:52:33 -0700

    Initial Commit

---
+++ Recommendations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recommendations
+//
+//  Created by Sarada Symonds on 7/28/15.
+//  Copyright (c) 2015 Sarada Symonds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
